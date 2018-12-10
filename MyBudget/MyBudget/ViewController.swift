464de1d0a57e04cbe27b53750a18ee7dfe51ea9d
hgao36 <32477364+hgao36@users.noreply.github.com>
2018-12-09 21:39:50 -0800

    Add files via upload

---
+++ MyBudget/MyBudget/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyBudget
+//
+//  Created by Huawei Gao on 2018/12/6.
+//  Copyright © 2018 Huawei Gao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
